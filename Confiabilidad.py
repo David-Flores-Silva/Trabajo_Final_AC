57229a6383542d07bcb0ba10c989d474a81cc9e7
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-15 18:36:32 -0500

    se agrego el nuevo archivo de confiabilidad que debe de reconocer los rostros de las personas

---
+++ Confiabilidad.py
@@ -0,0 +1,61 @@
+import cv2
+import os
+ 
+dataPath = "D:/DAVID/UNSA/IV TAREAS/9. labo de AC/Trabajo_Final_AC/Data"
+imagePaths = os.listdir(dataPath)
+print("imagePaths= ", imagePaths)
+ 
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer = cv2.face.FisherFaceRecognizer_create()
+ 
+#leendo el modelo
+#face_recognizer.read("modeloEigenFace.xml")
+face_recognizer.read("modeloFisherFace.xml")
+ 
+#cap = cv2.VideoCapture("img y videos/David.mp4")
+cap = cv2.VideoCapture("img y videos/Nicolas.mp4")
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+ 
+while True:
+    ret, frame = cap.read()
+    if ret == False:
+        break
+ 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = gray.copy()
+ 
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+ 
+    for (x, y, w, h) in faces:
+        rostro = auxFrame[y:y+h, x:x+w]
+        rostro = cv2.resize(rostro, (150, 150), interpolation = cv2.INTER_CUBIC)
+        result = face_recognizer.predict(rostro)
+ 
+        cv2.putText(frame, "{}".format(result),(x,y+5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+ 
+        #1
+        #if result[1] < 5700:
+        #    cv2.putText(frame, "{}".format(imagePaths[result[0]]),(x,y+5),1,1.3,(0,255,0),1,cv2.LINE_AA)
+        #    cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
+        #else:
+        #    cv2.putText(frame, "Desconocido",(x,y+5),1,1.3,(0,0,255),1,cv2.LINE_AA)
+        #    cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 0, 255), 2)
+    
+        #2
+        if result[1] < 500:
+            cv2.putText(frame, "{}".format(imagePaths[result[0]]),(x,y+5),1,1.3,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, "Desconocido",(x,y+5),1,1.3,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 0, 255), 2)
+ 
+ 
+ 
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
--- EntrenandoRF.py
+++ EntrenandoRF.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 
-dataPath = ""
+dataPath = "D:/DAVID/UNSA/IV TAREAS/9. labo de AC/Trabajo_Final_AC/Data"
 peopleList = os.listdir(dataPath)
 print("lista de personas: ", peopleList)
 
@@ -25,3 +25,18 @@ for nameDir in peopleList:
     
     label += 1
 
+#print("labels: ",labels)
+#print("numero de etiquetas 0: ",np.count_nonzero(np.array(labels)==0))
+#print("numero de etiquetas 0: ",np.count_nonzero(np.array(labels)==1))
+ 
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer = cv2.face.FisherFaceRecognizer_create()
+ 
+print("entrenamiento ... ")
+face_recognizer.train(facesData, np.array(labels))
+ 
+
+#face_recognizer.write("modeloEigenFace.xml")
+face_recognizer.write("modeloFisherFace.xml")
+print("modelo almacenado")
